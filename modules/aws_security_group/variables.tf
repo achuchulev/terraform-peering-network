c47eb98a33caa2d9e4547963518fa592ef76ca97
Atanas Chuchulev <atanas.v4@gmail.com>
2020-12-09 16:46:33 +0200

    Create variables.tf

---
+++ modules/aws_security_group/variables.tf
@@ -0,0 +1,20 @@
+variable "tags" {
+  type        = map(string)
+  description = "VPC tag"
+
+  default = {
+    Name = ""
+  }
+}
+
+variable "ssh_port" {
+  default = 22
+}
+
+variable "ssh_cidr" {
+  default = "0.0.0.0/0"
+}
+
+variable "icmp_cidr" {
+  default = "0.0.0.0/0"
+}
